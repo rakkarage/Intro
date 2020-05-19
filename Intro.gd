1e8a45083942b9794c6369bef96e2d03ab041348
Rakka Rage <rakkarage@gmail.com>
2020-05-19 14:47:10 -0400

    x

--- Intro.gd
+++ Intro.gd
@@ -5,22 +5,21 @@ const _delay0 = 0.333
 const _delay1 = 3.0
 const _delay2 = 0.666
 
-func _ready():
+func _ready() -> void:
 	$Tween.interpolate_property($Back, "color", Color8(59, 67, 82, 255), Color.black, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
 	$Tween.interpolate_property($Texture, "rect_rotation", 0, 180, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
 	$Tween.interpolate_property($Texture, "rect_scale", Vector2.ONE, Vector2.ONE * 4, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
 	$Tween.interpolate_property($Fore, "color", Color(0, 0, 0, 0), Color.black, _duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, _delay1)
 	$Tween.start()
-	Utility.ok($Tween.connect("tween_all_completed", self, "done"))
+	Utility.ok($Tween.connect("tween_all_completed", self, "_done"))
 	$Timer.start(_delay2)
-	Utility.ok($Timer.connect("timeout", self, "_timeout"))
+	yield($Timer, "timeout")
+	$Audio.play()
 
-func _timeout(): $Audio.play()
+func _input(event) -> void: if event.is_pressed(): _done()
 
-func _input(event): if event.is_pressed(): done()
+func _unhandled_input(_event) -> void: _done()
 
-func _unhandled_input(_event): done()
-
-func done():
+func _done() -> void:
 	get_tree().set_input_as_handled()
 	queue_free()
