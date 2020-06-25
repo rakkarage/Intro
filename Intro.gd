914bca692ed019c484db31c8b8ea709353ea219e
Rakka Rage <rakkarage@gmail.com>
2020-06-24 23:22:41 -0400

    x

--- Intro.gd
+++ Intro.gd
@@ -1,9 +1,9 @@
 extends Control
 
 const _duration = 1.0
-const _delay0 = 0.333
+const _delay0 = 0.5
 const _delay1 = 3.0
-const _delay2 = 0.666
+const _delay2 = 0.8
 
 onready var _back:    = $Back
 onready var _texture: = $Texture
@@ -15,7 +15,7 @@ onready var _audio:   = $Audio
 func _ready() -> void:
 	_tween.interpolate_property(_back, "color", Color8(59, 67, 82, 255), Color.black, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
 	_tween.interpolate_property(_texture, "rect_rotation", 0, 180, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
-	_tween.interpolate_property(_texture, "rect_scale", Vector2.ONE, Vector2.ONE * 4, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
+	_tween.interpolate_property(_texture, "rect_scale", Vector2.ONE, Vector2.ONE * 3, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
 	_tween.interpolate_property(_fore, "color", Color(0, 0, 0, 0), Color.black, _duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, _delay1)
 	_tween.start()
 	Utility.ok(_tween.connect("tween_all_completed", self, "_done"))
