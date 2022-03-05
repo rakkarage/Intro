14f241f30d4494bca01a28a8a6afb8081129b7c7
rakkarage <rakkarage@gmail.com>
2022-03-05 10:01:02 -0500

    x

--- Intro.gd
+++ Intro.gd
@@ -13,12 +13,12 @@ onready var _timer:   = $Timer
 onready var _audio:   = $Audio
 
 func _ready() -> void:
+	assert(_tween.connect("tween_all_completed", self, "_done") == OK)
 	_tween.interpolate_property(_back, "color", Color8(59, 67, 82, 255), Color.black, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
 	_tween.interpolate_property(_texture, "rect_rotation", 0, 180, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
 	_tween.interpolate_property(_texture, "rect_scale", Vector2.ONE, Vector2.ONE * 3, _duration, Tween.TRANS_BOUNCE, Tween.EASE_OUT, _delay0)
 	_tween.interpolate_property(_fore, "color", Color(0, 0, 0, 0), Color.black, _duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, _delay1)
 	_tween.start()
-	assert(_tween.connect("tween_all_completed", self, "_done") == OK)
 	_timer.start(_delay2)
 	yield(_timer, "timeout")
 	_audio.play()
