9d2d1dbe32dde63872e5a491b12dba8e975124c9
Rakka Rage <rakkarage@gmail.com>
2024-10-01 19:30:24 -0400

    x

--- Intro.gd
+++ Intro.gd
@@ -1,26 +1,34 @@
 extends Control
 
 @onready var _back: ColorRect = $Back
-@onready var _texture: TextureRect = $Texture
+@onready var _logo: TextureRect = $Texture
+@onready var _godot: TextureRect = $Godot
 @onready var _fore: ColorRect = $Fore
 @onready var _audio: AudioStreamPlayer = $Audio
 
-const _duration = 1.0
-const _delay_0 = 0.5
-const _delay_1 = 1.5
-const _delay_2 = 0.8
+const DURATION := 1.0
+const DELAY_0 := 0.5
+const DELAY_1 := 1.5
+const DELAY_2 := 0.8
+
 var _tween: Tween
+var _tween_godot: Tween
 
 func _ready() -> void:
+	var target = _godot.global_position.x
+	_godot.global_position.x = -DisplayServer.window_get_size().x
+	_tween_godot = create_tween()
+	_tween_godot.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+	_tween_godot.tween_property(_godot, "global_position:x", target, DURATION)
 	_tween = create_tween()
-	_tween.tween_interval(_delay_0)
+	_tween.tween_interval(DELAY_0)
 	_tween.set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
-	_tween.tween_property(_back, "color", Color.BLACK, _duration)
-	_tween.parallel().tween_property(_texture, "rotation", PI, _duration)
-	_tween.parallel().tween_property(_texture, "scale", Vector2.ONE * 3, _duration)
-	_tween.chain().tween_property(_fore, "color", Color.BLACK, _duration).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT).set_delay(_delay_1)
+	_tween.tween_property(_back, "color", Color.BLACK, DURATION)
+	_tween.parallel().tween_property(_logo, "rotation", PI, DURATION)
+	_tween.parallel().tween_property(_logo, "scale", Vector2.ONE * 3, DURATION)
+	_tween.chain().tween_property(_fore, "color", Color.BLACK, DURATION).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT).set_delay(DELAY_1)
 	_tween.tween_callback(_done)
-	await get_tree().create_timer(_delay_2).timeout
+	await get_tree().create_timer(DELAY_2).timeout
 	_audio.play()
 
 func _input(event: InputEvent) -> void:
@@ -29,5 +37,6 @@ func _input(event: InputEvent) -> void:
 	get_viewport().set_input_as_handled()
 
 func _done() -> void:
+	_tween_godot.kill()
 	_tween.kill()
 	queue_free()
